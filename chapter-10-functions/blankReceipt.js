7f3a2c791d27ab197a0da5a082a4da94d8d5af4c
Ivan <ivan.halkov@gmail.com>
2020-07-18 09:11:33 +0200

    Create blankReceipt.js

---
+++ chapter-10-functions/blankReceipt.js
@@ -0,0 +1,28 @@
+function blankReceipt() {
+    "use strict";
+    function printReceiptHeader() {
+        console.log("CASH RECEIPT");
+        console.log("------------------------------");
+    }
+
+    function printReceiptBody() {
+        console.log("Charged to____________________");
+        console.log("Received by___________________");
+    }
+
+    function printReceiptFooter() {
+        console.log("------------------------------");
+        console.log("(c) SoftUni");
+    }
+
+
+    function printReceipt() {
+        printReceiptHeader();
+        printReceiptBody();
+        printReceiptFooter();
+    }
+
+    printReceipt();
+}
+
+blankReceipt();
